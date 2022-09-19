e3f8c04d4862b872aec2501b22c4cf6e040ee4cd
Winnie Lu <winnie.lu@duke.edu>
2022-09-19 13:05:47 -0400

    added tests for check_LDL and check_total

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -9,16 +9,22 @@ def test_check_HDL(HDL_input, expected):
     answer = check_HDL(HDL_input)
     assert answer == expected
 
-'''
-def test_check_HDL_BorderlineLow():
-    from blood_calculator import check_HDL
-    answer = check_HDL(50)
-    expected = "Borderline Low"
+@pytest.mark.parametrize("LDL_input, expected",
+    [(85, "Normal"),
+    (150, "Borderline High"),
+    (170, "High"),
+    (200, "Very High")])
+def test_check_LDL(LDL_input, expected):
+    from blood_calculator import check_LDL
+    answer = check_LDL(LDL_input)
     assert answer == expected
 
-def test_check_HDL_High():
-    from blood_calculator import check_HDL
-    answer = check_HDL(20)
-    expected = "Low"
+@pytest.mark.parametrize("total_input, expected",
+    [(85, "Normal"),
+    (220, "Borderline High"),
+    (250, "High")])
+def test_check_total(total_input, expected):
+    from blood_calculator import check_total
+    answer = check_total(total_input)
     assert answer == expected
-'''
+
