bb77c3e0b1a0331ec060d1a21aea1bb4f801eed4
Winnie Lu <winnie.lu@duke.edu>
2022-09-26 12:50:34 -0400

    made databse a dictionary but there's a key error

--- database.py
+++ database.py
@@ -7,31 +7,27 @@ def create_patient_entry(patient_first_name, patient_last_name, patient_id, pati
     return new_patient
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 1, 30))
-    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
-    find_patient(db, 2)
+    db = {}
+    db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
+    find_patient(db, 22)
     add_test(db, 2, "LDL", 200)
     print_db(db)
-    print("Patient {} is a {}".format(get_full_name(db[2]), adult_or_minor(db[2])))
+    #print("Patient {} is a {}".format(get_full_name(db[2]), adult_or_minor(db[2])))
 
 def print_db(db):
     for patient in db:
-        print("Patient Name: " + get_full_name(patient) + ", " + 
-        "Patient ID: " + str(patient["ID"]) + ", " + 
-        "Patient Age: " + str(patient["Age"]) + ", " +
-        "Tests: " + str(patient["Tests"]))
+        print(patient)
+        print("Name: {}, id: {}, age: {}".format(get_full_name(patient), db[patient]["ID"], db[patient]["Age"]))
 
 def get_full_name(patient):
     full_name = "{} {}".format(patient["First Name"], patient["Last Name"])
     return full_name
 
 def find_patient(db, id):
-    for patient in db:
-        if patient["ID"] == id:
-            return patient
-    return False
+    patient = db[id]
+    return patient
 
 def adult_or_minor(patient):
     if patient["Age"] >= 18:
